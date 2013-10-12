2c5b4d63d651e17f04f505904600bfdd0eca996b
Melanie Archer <job@twobanjos.com>
2013-10-11 17:44:53 -0700

    Hmm. One get_sources won't return proper data, unless I filter by url, since the sources are formatted differently.

--- cheeses.rb
+++ cheeses.rb
@@ -1,13 +1,20 @@
 # Encoding: utf-8
 
-require 'rubygems'
+# require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-def get_cheeses(url)
-  cheeses = Nokogiri::HTML(open(url))
-
-  cheeses.css('#mw-content-text ul li a[title]').map(&:text)
+def get_sources(url)
+  sources = Nokogiri::HTML(open(url))
+  sources.css('#mw-content-text ul li a[title]').map(&:text)
 end
 
-puts get_cheeses('https://en.wikipedia.org/wiki/List_of_Italian_cheeses')
+# # First Name
+# puts get_sources('http://en.wikipedia.org/wiki/County_flowers_of_the_United_Kingdom')
+
+# # Middle Name
+# puts get_sources('http://en.wikipedia.org/wiki/List_of_Italian_cheeses')
+# puts get_sources('http://en.wikipedia.org/wiki/One_Hundred_Years_War')
+
+# Last Name
+puts get_sources('http://en.wikipedia.org/wiki/List_of_religious_houses_in_Scotland')
