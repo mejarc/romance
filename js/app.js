03b9e48ed05773594d5aa7cfb3feb51e47728e3a
Melanie Archer <job@twobanjos.com>
2013-10-28 14:21:31 -0700

    Yay. JS now grabs random line from Ruby-created file.

---
+++ js/app.js
@@ -0,0 +1,56 @@
+/* Author: Melanie Archer, twobanjos.com */
+
+(function() {
+    "use strict";
+    var toggler, txName;
+    toggler = $('.toggled');
+    txName = $("#output input[type=text]");
+/** 
+* Opens Ruby-generated text file, reads
+* each line, places data as value on form
+* input.
+* @param {String} txdata
+*/
+    function handleAuthor(txdata) {
+        var lines = txdata.split("\n"),
+            names = [],
+            i,
+            j,
+            len = lines.length;
+        for (i = 0; i<len; i+=1) {
+            names.push(lines[i]);
+        }
+        j = Math.floor(Math.random() * (len - 1));
+        txName.attr("value", names[j]);
+    }
+
+    function moveButton(whichButton, where, newTxt) {
+        var $btn, $where,v;
+        $btn = $(whichButton);
+        $where = $(where);
+        if ($btn.filter(':visible')) {
+            v = $btn.val();
+$btn.hide().val(newTxt).insertAfter($where).addClass('secondary').show();
+ $where.parent('li').slideDown('fast');
+        }
+        else {
+            $btn.show();
+        }
+   }
+
+
+    $(document).delegate('#generate', 'click', function(evt, data) {
+        evt.preventDefault();
+        $.ajax({
+            type: "GET",
+            dataType: "text",
+            url: "/romance/js/names.json",
+            success: function(data) {
+                toggler.slideDown('fast');
+                handleAuthor(data);
+                moveButton('#generate', '#wall', 'Generate your next author name');
+            }
+        });
+    });
+})();
+
