fba0d1cc41ddfb7f3810b1934970cb357e046aa4
Melanie Archer <job@twobanjos.com>
2014-03-30 17:56:45 -0700

    Putting in the rest of Sinatra.

--- Gemfile
+++ Gemfile
@@ -1,9 +1,37 @@
 source 'https://rubygems.org'
 ruby '1.9.3'
 
-gem 'rake'
+# PostgreSQL driver
+gem 'pg'
+
+# Sinatra driver
 gem 'sinatra'
 gem 'sinatra-contrib'
+
+# Use Thin for our web server
+gem 'thin'
+
+gem 'activesupport'
+gem 'activerecord'
+
+gem 'rake'
+
+gem 'shotgun'
+
+gem 'json'
+
 gem 'omniauth'
 gem 'omniauth-facebook'
 gem 'fb_graph'
+
+group :test do
+  gem 'shoulda-matchers'
+  gem 'rack-test'
+  gem 'rspec'
+  gem 'capybara'
+end
+
+group :test, :development do
+  gem 'factory_girl'
+  gem 'faker'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+
+# All our specs should require 'spec_helper' (this file)
+
+# If RACK_ENV isn't set, set it to 'test'.  Sinatra defaults to development,
+# so we have to override that unless we want to set RACK_ENV=test from the
+# command line when we run rake spec.  That's tedious, so do it here.
+ENV['RACK_ENV'] ||= 'test'
+
+require File.expand_path("../../config/environment", __FILE__)
+require 'shoulda-matchers'
+require 'rack/test'
+require 'factory_girl'
+
+FactoryGirl.definition_file_paths = %w{./factories ./test/factories ./spec/factories}
+FactoryGirl.find_definitions
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+  config.include FactoryGirl::Syntax::Methods
+  config.color_enabled = true
+  config.tty = true
+  config.formatter = :documentation # :progress, :html, :textmate
+end
+
+def app
+  Sinatra::Application
+end
