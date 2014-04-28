286ae2399cacaced2d93828ccee1cd448892b6a4
Melanie Archer <job@twobanjos.com>
2014-04-27 22:06:20 -0700

    Adding RSpec, Capybara, Jasmine

---
+++ spec/features/show_hide_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe "Generating and displaying author name: ", js: true do
+  context "home page: " do
+    before :all do
+      visit "/"
+    end
+
+    it "hides the author name text input on page load" do
+      expect(page).to have_css('#reporter', visible: false)
+    end
+
+    it "shows the author name text input on button click" do
+      click_button "Generate your author name"
+      expect(page.find_field('reporter').visible?).to be_true
+    end
+
+    it "fills in the author name text input on button click" do
+      click_button "Generate your author name"
+      expect(find_field('reporter').value.blank?).to be_false
+    end
+
+    
+    # it "shows the author name text input after the pseudonym is fetched" do
+    #   
+    # end
+
+
+    # it "replaces the placeholder on the text input" do
+      
+    #   expect(page).to have_no_content("just a sec")
+    # end
+    # it ""
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,9 @@
 require 'rubygems'
-
-# All our specs should require 'spec_helper' (this file)
-
-# If RACK_ENV isn't set, set it to 'test'.  Sinatra defaults to development,
-# so we have to override that unless we want to set RACK_ENV=test from the
-# command line when we run rake spec.  That's tedious, so do it here.
 ENV['RACK_ENV'] ||= 'test'
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'shoulda-matchers'
+require 'capybara'
 require 'rack/test'
 require 'factory_girl'
 
@@ -18,11 +13,13 @@ FactoryGirl.find_definitions
 RSpec.configure do |config|
   config.include Rack::Test::Methods
   config.include FactoryGirl::Syntax::Methods
+  config.include Capybara::DSL
   config.color_enabled = true
-  config.tty = true
   config.formatter = :documentation # :progress, :html, :textmate
 end
 
 def app
   Sinatra::Application
 end
+
+Capybara.app = app
